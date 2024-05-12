8ae616b0e054f8142139f25edf39e1081c1be2ee
swetaprangya <105152167+swetaprangya@users.noreply.github.com>
2024-05-12 16:21:14 +0530

    Add files via upload

---
+++ PRO-C214-StudentBoilerplate-main/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $("#show_chat").click(function(){
+        $(".right-window").css("display","block")
+        $(".left-window").css("display","none")
+        $(".header_back").css("display","block")
+    })
+    $(".header_back").click(function(){
+        $(".right-window").css("display","none")
+        $(".left-window").css("display","block")
+        $(".header_back").css("display","none")
+    })
+})
